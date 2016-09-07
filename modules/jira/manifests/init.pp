605ce477ae3c2a0f5a1bdb06f452e755dbe4279d
root <root@master>
2016-09-07 15:14:09 +0100

    Completed Jira installation

--- modules/jira/manifests/init.pp
+++ modules/jira/manifests/init.pp
@@ -2,24 +2,48 @@ class jira {
 	$javahome = '/usr/lib/jvm/java-7-openjdk-amd64'
 	
 	exec { 'Download jira' :
-		command => 'wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-6.4.14-war.tar.gz',
+		command => 'wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.2.1-x64.bin',
 		timeout => 180000,
-		creates => '/etc/atlassian-jira-6.4.14-war.tar.gz',
+		creates => '/opt/atlassian-jira-software-7.2.1-x64.bin',
 		user => root,
-		cwd => '/etc',
+		cwd => '/opt',
 		path => ['/usr/bin', '/bin'],
 	} ->
-	
-	#exec { 'Move the tar' :
-	#	command => 'cp atlassian-jira-6.4.14-war.tar.gz /opt/',
+
+	exec { 'make accessible' :
+		command => 'chmod a+x atlassian-jira-software-7.2.1-x64.bin',
+		user => root,
+		cwd => '/opt',
+		path => ['/usr/bin', '/bin'],
+	} ->
+
+	exec { 'Install jira' :
+		command => '/opt/atlassian-jira-software-7.2.1-x64.bin -q',
+		timeout => 180000,
+		user => root,
+		creates => '/opt/JIRA Software'
+	} #->
+
+	#exec { 'Swap jiras ports' :
+	#	command => 'perl -pi -e "s/<Server port="8005" shutdown="SHUTDOWN">/<Server port="8006" shutdown="SHUTDOWN">/g" server.xml && perl -pi -e "s/<Connector port="8080"/<Connector port="8081"/g" server.xml',
+	#	timeout => 180000,
 	#	user => root,
+	#	cwd => '/opt/JIRA Software/conf/',
 	#	path => ['/usr/bin', '/bin'],
 	#} ->
 
-	exec { 'Extract the tar' :
-		command => 'tar -xzf atlassian-jira-6.4.14-war.tar.gz',
-		user => root,
-		cwd => '/etc',
-		path => ['/usr/bin', '/bin'],
-	}
+	#exec { 'Stop jira' :
+	#	command => './stop-jira.sh',
+	#	user => root,
+	#	path => ['/opt/JIRA Software/bin/', '/bin'],
+	#} ->
+
+	#exec { 'Start jira' :
+	#	command => './start-jira.sh',
+	#	user => root,
+	#	path => ['/opt/JIRA Software/bin/', '/bin'],
+	#}
+
+
+
 }
