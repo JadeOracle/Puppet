d5c5eee74ca999b8eeba444d0999c89e5b70ed0c
root <root@master>
2016-09-08 09:51:23 +0100

    Un-mew

---
+++ modules/setup/manifests/init.pp
@@ -0,0 +1,28 @@
+class setup {
+	exec { 'make java7 available':
+		command => 'add-apt-repository ppa:openjdk-r/ppa',
+		user => root,
+		path => [ '/usr/bin', '/bin/' ],
+	} ->
+	
+	class { 'java':
+  		package => 'openjdk-7-jdk',
+	} ->
+	
+	class { 'jenkins':
+  	} ->
+
+	class { 'jira':
+  	}
+
+	
+	#package { 'git' :
+	#	ensure => present,
+	#} ->
+
+	#class{ 'nexus':
+    	#	version    => '2.8.0',
+    	#	revision   => '05',
+    	#	nexus_root => '/srv',
+  	#}
+}
