a4d0b931de6f83cd962b2f2ee1f90e8a91b73cdd
Thatdwarf <dominic.goldsmith@hotmail.co.uk>
2016-09-08 09:59:56 +0100

    Update init.pp

--- modules/setup/manifests/init.pp
+++ modules/setup/manifests/init.pp
@@ -1,12 +1,12 @@
 class setup {
-	exec { 'make java7 available':
-		command => 'add-apt-repository ppa:openjdk-r/ppa',
-		user => root,
-		path => [ '/usr/bin', '/bin/' ],
-	} ->
 	
 	class { 'java':
-  		package => 'openjdk-7-jdk',
+	} ->
+	
+	class { 'maven':
+	} ->
+	
+	class { 'git':
 	} ->
 	
 	class { 'jenkins':
@@ -14,11 +14,6 @@ class setup {
 
 	class { 'jira':
   	}
-
-	
-	#package { 'git' :
-	#	ensure => present,
-	#} ->
 
 	#class{ 'nexus':
     	#	version    => '2.8.0',
