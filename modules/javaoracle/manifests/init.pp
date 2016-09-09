fcf28f57d7ff39fcdb41b46b8ed28dccdf712507
JadeOracle <jadenoracle@gmail.com>
2016-09-09 10:16:16 +0100

    Update init.pp

--- modules/javaoracle/manifests/init.pp
+++ modules/javaoracle/manifests/init.pp
@@ -3,7 +3,13 @@ class javaoracle {
 		command => 'add-apt-repository ppa:webupd8team/java && apt-get update',
 		user => root,
 		path => [ '/usr/bin', '/bin/' ],
-	}
+	} ->
+	
+	exec { 'accept license':
+		command => 'echo debconf shared/accepted-oracle-license-v1-1 select true | \  sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | \ sudo debconf-set-selections',
+		user => root,
+		path => [ '/usr/bin', '/bin/' ],
+	} -> 
 		
 	package {'oracle-java7-installer':
 		ensure 	=> present,
