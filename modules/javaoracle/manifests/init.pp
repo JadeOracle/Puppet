1ace5dd14f03a2b89dd65b01521a940165d92219
JadeOracle <jadenoracle@gmail.com>
2016-09-09 10:24:21 +0100

    Update init.pp

--- modules/javaoracle/manifests/init.pp
+++ modules/javaoracle/manifests/init.pp
@@ -5,8 +5,12 @@ class javaoracle {
 		path => [ '/usr/bin', '/bin/' ],
 	} ->
 	
+	package { 'debconf-utils':
+		ensure 	=> present,
+	} ->
+	
 	exec { 'accept license':
-		command => 'echo debconf shared/accepted-oracle-license-v1-1 select true | \  sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | \ sudo debconf-set-selections',
+		command => 'echo debconf shared/accepted-oracle-license-v1-1 select true | \ debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | \ debconf-set-selections',
 		user => root,
 		path => [ '/usr/bin', '/bin/' ],
 	} -> 
