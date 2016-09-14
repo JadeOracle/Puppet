9cc7cf1cf592271285b13da10bfb632ce1580fa5
root <root@master>
2016-09-14 10:11:58 +0100

    Configured mc server

--- modules/mcollective/manifests/init.pp
+++ modules/mcollective/manifests/init.pp
@@ -2,7 +2,6 @@ class mcollective {
   # Install
   package {'mcollective':
     ensure => latest,
-    require => service['activemq']
   }
 
   # Run
@@ -11,6 +10,42 @@ class mcollective {
     enable  => true,
     require => Package['mcollective'],
   }
+
+	file { 'server_private.pem':
+		ensure => present,
+		path => '/etc/mcollective/server_private.pem',
+		source => '/var/lib/puppet/ssl/private_keys/agent.pem',
+		require => Package['mcollective'],
+	}
+
+	file { 'server_public.pem':
+		ensure => present,
+		path => '/etc/mcollective/server_public.pem',
+		source => '/var/lib/puppet/ssl/public_keys/agent.pem',
+		require => Package['mcollective'],
+	}
+
+	file { 'clients':
+		ensure => directory,
+		path => '/etc/mcollective/clients',
+		require => Package['mcollective'],
+	}
+
+	file { 'ca.pem':
+		ensure => present,
+		path => '/etc/mcollective/clients/ca.pem',
+		source => '/var/lib/puppet/ssl/certs/ca.pem',
+		require => [file['clients'], Package['mcollective']],
+	}
+
+	file{"/etc/mcollective/facts.yaml":
+      		owner    => root,
+      		group    => root,
+      		mode     => 400,
+      		loglevel => debug, # reduce noise in Puppet reports
+      		content  => inline_template("<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>"), # exclude rapidly changing facts
+		require => Package['mcollective'],
+    	}
 
   # Restart the service when any settings change
   #Package['mcollective'] -> Mcollective::Setting <| |> ~> Service['mcollective']
